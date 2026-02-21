84e406c552496ebdd30799903747b33e3c01bf5d
Helal366 <helal56876@gmail.com>
2026-02-21 15:17:15 +0600

    doubly linked list operations: insert at any position is updated.

--- doubly_link_list/operations/insert_at_any_position.cpp
+++ doubly_link_list/operations/insert_at_any_position.cpp
@@ -20,41 +20,54 @@ void print_forward(Node* head){
     }
     cout << endl;
 }
-void insert_at_any_position(Node* &head, int idx, int val){
-    if(idx<0){
-        cout << "Invalid index" << endl;
+void insert_at_any_position(Node* &head, Node* &tail, int idx, int val){
+   if(idx<0){
+    cout << "Invalid index\n";
+    return;
+   }
+   Node* newNode=new Node(val);
+    // empty list
+   if(head==NULL){
+    if(idx!=0){
+        cout << "List is empty.\n";
+        delete newNode;
         return;
     }
-    Node* newnode=new Node(val);
-    if(idx==0){
-        if(head!=NULL){
-            newnode->next=head;
-            head->prev=newnode;
-        }
-        head=newnode;
+    head=tail=newNode;
+    return;
+   }
+   // insert at head
+   if(idx==0){
+        head->prev=newNode;
+        newNode->next=head;
+        head=newNode;
+        return;
+   }
+   Node* tmp= head;
+   // traverse to (idx-1)
+   for(int i=0; i<idx-1; i++){
+    if(tmp==NULL){
+        cout << "Invalid index\n";
+        delete newNode;
         return;
     }
-    Node* temp=head;
-    for (int i = 0; i < idx-1; i++)
-    {
-        if(temp==NULL){
-            cout << "Invalid index" << endl;
-            return;
-        }
-        temp=temp->next;
-    }
-    if(temp==NULL){
-        cout << "Invalid index" << endl;
+    tmp=tmp->next;
+   }
+   if(tmp==NULL){
+        cout << "Invalid index\n";
+        delete newNode;
         return;
-    }
-    newnode->next=temp->next;
-    if(temp->next!=NULL){
-        temp->next->prev=newnode;
-    }
-    temp->next=newnode;
-    newnode->prev=temp;
-    
+   }
+   newNode->next=tmp->next;
+   newNode->prev=tmp;
+   tmp->next=newNode;
+   if(newNode->next==NULL){
+       tail=newNode;
+    }else{
+       newNode->next->prev=newNode;
+   }
 }
+
 int main(){
     Node* head=new Node(10);
     Node* a=new Node(20);
@@ -69,11 +82,11 @@ int main(){
 
     b->next=tail;
     tail->prev=b;
-    insert_at_any_position(head, 2, 100);
-    insert_at_any_position(head, 1, 200);
-    insert_at_any_position(head, 0, 300);
-    insert_at_any_position(head, 3, 400);
-    insert_at_any_position(head, 0, 500);
+    insert_at_any_position(head,tail, 2, 100);
+    insert_at_any_position(head,tail, 1, 200);
+    insert_at_any_position(head,tail, 0, 300);
+    insert_at_any_position(head,tail, 3, 400);
+    insert_at_any_position(head,tail, 0, 500);
     print_forward(head);
     return 0;
 }
