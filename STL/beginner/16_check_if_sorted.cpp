092602e9687fcfffb9893a906115642a3973c26c
Helal366 <helal56876@gmail.com>
2026-03-06 23:08:48 +0600

    one more problem is solved.

--- STL/beginner/16_check_if_sorted.cpp
+++ STL/beginner/16_check_if_sorted.cpp
@@ -2,3 +2,32 @@
 // Problem:
 // Check if a list is sorted in ascending order.
 // Output YES or NO.
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n;
+    list<int> li;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        li.push_back(x);
+    }
+    bool flag = true;
+    if(!li.size() > 1){
+        auto prev = li.begin();
+        auto curr = next(prev);
+        while (curr != li.end())
+        {
+            if (*prev > *curr)
+            {
+                flag = false;
+                break;
+            }
+            ++prev;
+            ++curr;
+        }
+    }
+    return 0;
+}
