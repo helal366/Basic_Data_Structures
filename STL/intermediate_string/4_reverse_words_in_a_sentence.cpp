21d53c475e33852035d395c904735b3c6cd89cd8
Helal366 <helal56876@gmail.com>
2026-03-07 12:19:08 +0600

    reverse string problem on list is solved.

---
+++ STL/intermediate_string/4_reverse_words_in_a_sentence.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string sentence;
+    getline(cin, sentence);
+    list<string>words;
+    string word;
+    stringstream ss(sentence);
+    while(ss >> word){
+        words.push_back(word);
+    }
+    words.reverse();
+    for(auto w:words) cout << w << " ";
+
+    return 0;
+}
